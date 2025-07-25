d546a79d800cff12612007cd3628b640d7c3651f
kiri173 <ST10446852@vcconnect.edu.za>
2025-07-25 11:33:20 +0200

    Added Ice task

---
+++ IceTask1/Object.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceTask1
+{
+    class Object
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Object(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Console.WriteLine($"Object Created: {Name} (ID: {Id})");
+        }
+        Object()
+        {
+            Console.WriteLine($"Object Finalized: {Name} (ID: {Id})");
+        }
+    }
+}
---
+++ IceTask1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IceTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Object obj1 = new Object(1, "Object 1");
+            Object obj2 = new Object(2, "Object 2");
+
+            
+            Console.WriteLine($"Using {obj1.Name} and {obj2.Name}");
+
+            
+            obj1 = null;
+            obj2 = null;
+
+            
+            Console.WriteLine("Forcing garbage collection...");
+
+            GC.Collect();
+
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine("Garbage collection complete.");
+
+            Console.ReadLine();
+        }
+    }
+}
